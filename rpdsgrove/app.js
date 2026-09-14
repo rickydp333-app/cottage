3ad19c8c6a0cfd5d2901f33f21baf15d43e9b221
rickydp333-app <rickydp333@gmail.com>
2026-09-13 20:08:39 -0400

    Place Return to kiosk beside Add song with native launcher return support

--- rpdsgrove/app.js
+++ rpdsgrove/app.js
@@ -69,3 +69,5 @@ const sdk=document.createElement('script');sdk.src='https://www.gstatic.com/cv/j
 if('serviceWorker'in navigator)navigator.serviceWorker.register('sw.js').catch(()=>{});
 
 $('#deviceVolume').onchange=e=>volume(Number(e.target.value));$('#fullscreen').onclick=async()=>{try{if(document.fullscreenElement)await document.exitFullscreen();else await document.documentElement.requestFullscreen()}catch{notice('Use your browser’s full-screen option, or install the app.')}};
+
+$('#returnToKiosk').onclick=()=>{if(document.documentElement.dataset.kioskReturn==='ready')window.dispatchEvent(new Event('rpdsgrove:return'));else location.assign('/')};
